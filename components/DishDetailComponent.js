44ef3f8910a5dca3b0c8e8b7a76e19c668799711
Josh Rebelez <59lpjrebelez@gmail.com>
2019-03-12 12:25:04 -0700

    share

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import { View,Text,ScrollView, FlatList,Modal, Button,StyleSheet,Alert, PanResponder } from 'react-native';
+import { View,Text,ScrollView, FlatList,Modal, Button,StyleSheet,Alert, PanResponder, Share } from 'react-native';
 import { Card,Icon,Rating,Input} from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -75,6 +75,15 @@ function RenderDish(props){
         } 
         
     });
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
 
     if ( dish != null){
 
@@ -106,6 +115,14 @@ function RenderDish(props){
                     color='#512DA8'
                     onPress={() => props.showModal() }
                     />
+                    <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
                     </View>
                 </Card>
                 </Animatable.View>
